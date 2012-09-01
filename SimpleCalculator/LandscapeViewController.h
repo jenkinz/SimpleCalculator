39a139530d4ea6ea3cbe2f3ad033ec3e966d6808
Brian Jenkins <bj@jenkinz.com>
2012-09-01 15:29:34 -0700

    Play around with landscape orientation

---
+++ SimpleCalculator/LandscapeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandscapeViewController.h
+//  SimpleCalculator
+//
+//  Created by Brian Jenkins on 9/1/12.
+//  Copyright (c) 2012 Brian Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandscapeViewController : UIViewController
+
+@end
